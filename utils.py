fe05e8b73db6ea5307968deabe645971887b1c72
Kuba <kubaszac089@gmail.com>
2026-05-28 12:05:18 +0200

    dodano: docstring, final newline

--- utils.py
+++ utils.py
@@ -1,12 +1,30 @@
-# przykladowe funkcje utils . py
-def add(a : int , b : int) -> int:
+"""
+podstawowe funkcje matematyczne
+"""
+
+def add(a: int, b: int) -> int:
+    """
+    dodawanie dwoch liczb calkowitych
+    """
     return a + b
 
-def subtract(a : int , b : int) -> int:
+
+def subtract(a: int, b: int) -> int:
+    """
+    odejmowanie liczb calkowitych: b od a
+    """
     return a - b
 
-def multiply(a : int , b : int) -> int:
+
+def multiply(a: int, b: int) -> int:
+    """
+    mnozenie dwoch liczb calkowitych
+    """
     return a * b
 
-def divide(a : int , b : int) -> float:
+
+def divide(a: int, b: int) -> float:
+    """
+    dzielenie liczb calkowitych- a przez b
+    """
     return a / b
